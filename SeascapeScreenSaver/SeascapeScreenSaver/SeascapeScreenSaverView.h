9666d3ce6716d586137a4b4abc3a8b125f1e2050
David Mitchell <david5.mitchell8@gmail.com>
2015-01-04 18:09:14 -0800

    Initial Commit

---
+++ SeascapeScreenSaver/SeascapeScreenSaver/SeascapeScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  SeascapeScreenSaverView.h
+//  SeascapeScreenSaver
+//
+//  Created by David Mitchell on 1/4/15.
+//  Copyright (c) 2015 David Mitchell. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SeascapeScreenSaverView : ScreenSaverView
+
+@end
---
+++ SeascapeScreenSaver/SeascapeScreenSaver/SeascapeScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  SeascapeScreenSaverView.m
+//  SeascapeScreenSaver
+//
+//  Created by David Mitchell on 1/4/15.
+//  Copyright (c) 2015 David Mitchell. All rights reserved.
+//
+
+#import "SeascapeScreenSaverView.h"
+
+@implementation SeascapeScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
