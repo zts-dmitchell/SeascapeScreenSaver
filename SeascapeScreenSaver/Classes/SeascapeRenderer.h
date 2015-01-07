01c8efc27d5b25c4d4a7b89bc71ccb823f964a4a
David Mitchell <david5.mitchell8@gmail.com>
2015-01-07 10:09:29 -0800

    Restructured files to match Xcode grouping.

---
+++ SeascapeScreenSaver/Classes/SeascapeRenderer.h
@@ -0,0 +1,55 @@
+//
+//  SeascapeRenderer.h
+//  Seascape
+//
+//  Created by David Mitchell on 2/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ESRenderer.h"
+#import "ShaderUtil.h"
+
+typedef struct SeascapeVec2 {
+    GLfloat x;
+    GLfloat y;
+}SeascapeVec2;
+
+typedef struct SeascapeVec3 {
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+}SeascapeVec3;
+
+typedef struct SeascapeBuffers {
+    GLuint VertexBuffer;
+}SeascapeBuffers;
+
+typedef struct SeascapeUniformHandles {
+    GLint m_u_iGlobalTimeHandle;
+    GLint m_u_iResolutionHandle;
+    GLint m_u_iSeaHeightHandle;
+    GLint m_u_iSeaChoppyHandle; // 0 to 4;
+    GLint m_u_iSpeedHandle;
+}SeascapeUniformHandles;
+
+typedef struct SeascapeAttributeHandles {
+    GLint m_a_posHandle;
+}SeascapeAttributeHandles;
+
+@interface SeascapeRenderer : NSObject <ESRenderer, Attributes>
+{
+@private
+    GLuint program;
+    SeascapeBuffers m_buffers;
+    SeascapeUniformHandles m_uniforms;
+    SeascapeAttributeHandles m_attributes;
+    
+    GLfloat m_u_iGlobalTime;
+    SeascapeVec3  m_u_iResolution;
+    SeascapeVec3 m_a_pos;
+    GLfloat m_u_iSeaChoppy;
+    GLfloat m_u_iSeaHeight;
+    GLfloat m_u_iSpeed;
+}
+
+@end
