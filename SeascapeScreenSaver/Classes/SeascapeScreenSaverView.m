0d5741a737e2d2120a16d866abfeebea05632928
David Mitchell <david5.mitchell8@gmail.com>
2015-01-09 22:17:18 -0800

    Added SymmetricOptics from Shadertoy.

--- SeascapeScreenSaver/Classes/SeascapeScreenSaverView.m
+++ SeascapeScreenSaver/Classes/SeascapeScreenSaverView.m
@@ -13,6 +13,8 @@
 #import "SeascapeRenderer.h"
 #import "WobblerRenderer.h"
 #import "MountainsRenderer.h"
+#import "SymmetricOriginsRenderer.h"
+
 #import <OpenGL/gl.h>
 
 @interface SeascapeScreenSaverView(PrivateMethods)
@@ -178,7 +180,8 @@ const int g_countOfRenderers = 2; // Disable the Mountains.3;
             
         default:
             NSLog(@"Unknown renderer"); // Fall through
-        case 0: self.renderer = [[SeascapeRenderer alloc] init];
+        //case 0: self.renderer = [[SeascapeRenderer alloc] init];
+        case 0: self.renderer = [[SymmetricOriginsRenderer alloc] init];
     }
     
     [self.renderer setFrameSize:self.screenSize];
