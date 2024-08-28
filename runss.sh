fc4e0d2c574c68f92caace932ef3cbcf0487f5ab
David Mitchell <david5.mitchell8@gmail.com>
2024-08-27 18:27:39 -0700

    Updates to the project file

--- runss.sh
+++ runss.sh
@@ -2,3 +2,4 @@
   
 #open /Users/dmitchell/Library/Developer/Xcode/DerivedData/SeascapeScreenSaver-cnwfwmupptfzkpfnatrqydxnhqqs/Build/Products/Debug/SeascapeScreenSaver.saver
 open /Users/dmitchell/Library/Developer/Xcode/DerivedData/SeascapeScreenSaver-epjzxgtqvjnojugyiljjoaevfvgo/Build/Products/Release/SeascapeScreenSaver.saver
+#open /Users/dmitchell/Library/Developer//Xcode/DerivedData/SeascapeScreenSaver-epjzxgtqvjnojugyiljjoaevfvgo/Build/Products/Release/SeascapeScreenSaver.saver
